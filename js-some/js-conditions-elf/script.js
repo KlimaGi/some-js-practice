47e266b2bad5beed0a2b651fa70e9ae03a9be7a1
Giedre <giedre.klima@gmail.com>
2022-05-31 18:15:22 +0300

    roko task done

--- js-some/js-conditions-elf/script.js
+++ js-some/js-conditions-elf/script.js
@@ -34,22 +34,96 @@
 // 1.
 //let age = Number(prompt("iveskite amziu"));
 
-let pElement = document.getElementById("#tx");
-// pElement.textContent = "Sveikas";
+//  day task 05-31 --- 0.3
+// let personName = "John";
+// let isLoggedIn = true;
+// let time = 21;
+// let isBirthday = false;
 
-// day task 05-31
-let personName = "John";
-let isLoggedIn = true;
-let time = 21;
-let isBirthday = false;
+// let dayPart;
+// if (time >= 5 && time <= 12) dayPart = "Morning";
+// else if (time >= 13 && time <= 18) dayPart = "Afternoon";
+// else if (time >= 19 || time <= 4) dayPart = "Evening";
+// let hello = `Good ${dayPart}`;
 
-let dayPart;
-if (time >= 5 && time <= 12) dayPart = "Morning";
-else if (time >= 13 && time <= 18) dayPart = "Afternoon";
-else if (time >= 19 || time <= 4) dayPart = "Evening";
-let hello = `Good ${dayPart}`;
+// if (isBirthday && isLoggedIn)
+//   console.log(hello.concat(`, ${personName} and have a great birthday!`));
+// else if (isLoggedIn) console.log(hello.concat(`, ${personName}.`));
+// else console.log(hello.concat("..."));
 
-if (isBirthday && isLoggedIn)
-  console.log(hello.concat(`, ${personName} and have a great birthday!`));
-else if (isLoggedIn) console.log(hello.concat(`, ${personName}.`));
-else console.log(hello.concat("..."));
+// 1.
+// let age = 18;
+// let hasCarRights = true;
+// let isDrunk = false;
+
+// if (age >= 18 && hasCarRights && !isDrunk) console.log("You can ride a car");
+// else console.log("You can't ride a car");
+
+// 2.
+// let personName = "Suzi";
+// let city = "Kaunas";
+// let hello = `Hello, ${personName}`;
+
+// let what = "";
+// //console.log(hello);
+// if (city === "Vilnius") what = "green";
+// else if (city === "Kaunas") what = "sunny";
+// else if (city === "Klaipeda") what = "rainy";
+// console.log(hello.concat(` from ${what} ${city}.`));
+
+//3.
+// let person = {
+//   name: "John",
+//   gender: "",
+//   age: 27,
+//   pet: "cat",
+// };
+// if (person.gender === "male") console.log(`Hello, Mr. ${person.name}`);
+// else if (person.gender === "female") console.log(`Hello, Ms. ${person.name}`);
+// else console.log(`Helo, ${person.name}`);
+
+//4.
+// let month = "05";
+// let season = "";
+// switch (month) {
+//   case "12":
+//   case "01":
+//   case "02":
+//     season = "winter";
+//     break;
+//   case "03":
+//   case "04":
+//   case "05":
+//     season = "spring";
+//     break;
+//   case "06":
+//   case "07":
+//   case "08":
+//     season = "summer";
+//     break;
+//   case "09":
+//   case "10":
+//   case "11":
+//     season = "autumn";
+//     break;
+//   default:
+//     console.log("lets write number of month");
+// }
+// console.log(season);
+
+// 5.
+let name = ["Brigita", "Vilte", "Lukas"];
+let personName = name[1];
+let result = personName
+  .split("")
+  .slice(0, personName.length - 1)
+  .join("");
+let last = personName
+  .split("")
+  .slice(personName.length - 1, personName.length)
+  .join();
+
+let ref = "";
+if (last === "s") ref = result.concat("i");
+else ref = personName;
+console.log(`Labas, ${ref}`);
