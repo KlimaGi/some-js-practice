6b63602a1dea7fae29e449c89a74ca9229ace1bc
Giedre <giedre.klima@gmail.com>
2022-06-07 12:06:26 +0300

    func and arr

--- js-some/js-5-methods/script.js
+++ js-some/js-5-methods/script.js
@@ -187,7 +187,7 @@ function listMoviesD(arr) {
     } </li>`;
   }
 }
-listMoviesD(moviesD);
+//listMoviesD(moviesD);
 
 // 14.1 dar 2 filmu arr, i ekrana isvesti kaip viena list'a
 const moviesD1 = [
@@ -203,4 +203,105 @@ const moviesD2 = [
 
 const oneArr = moviesD.concat(moviesD1).concat(moviesD2);
 console.log(oneArr);
-listMoviesD(oneArr);
+//listMoviesD(oneArr);
+
+// movies with arr
+let moviesWithYearList = [
+  [`filmas1`, 2001],
+  [`filmas2`, 2011],
+  [`filmas3`, 2009],
+  [`filmas4`, 2016],
+  [`filmas5`],
+  [`filmas6`, 2021],
+];
+
+function moviesWithYearList2(moviesarr) {
+  let moviesListElement = document.querySelector("ul");
+
+  for (let i = 0; i < moviesarr.length; i++) {
+    let num = i + 1;
+    let movieName = moviesarr[i][0];
+    //let movieYear = moviesarr[i][1] || "";
+    let movieYear = moviesarr[i][1] ? `(${moviesarr[i][1]})` : "";
+
+    moviesListElement.innerHTML += `<li>${num}. ${movieName} ${movieYear} </li>`;
+  }
+}
+//moviesWithYearList2(moviesWithYearList);
+
+// more arr
+let movies1 = [
+  "filmas1",
+  "filmas2",
+  "filmas3",
+  "filmas4",
+  "filmas5",
+  "filmas6",
+];
+let movies2 = ["filmas11", "filmas12", "filmas13"];
+let movies3 = ["filmas14", "filmas15", "filmas16"];
+
+//let allM = [];
+//allM.push(movies1, movies2);
+let allM = [movies1, movies2];
+console.log(allM);
+console.log(allM.length);
+
+function nestedArr(moviesArr) {
+  let moviesListElement = document.querySelector("ul");
+  let movieNum = 1;
+  for (let i = 0; i < moviesArr.length; i++) {
+    let innerMoviesArr = moviesArr[i];
+
+    for (let j = 0; j < innerMoviesArr.length; j++) {
+      let movieName = innerMoviesArr[j];
+      console.log("movieName", movieName);
+      moviesListElement.innerHTML += `<li>${movieNum}. ${movieName}</li>`;
+      movieNum++;
+    }
+  }
+}
+//nestedArr(allM);
+
+// with just arr
+console.log("allM", allM);
+let allMoviesConcat = movies1.concat(movies2, movies3);
+console.log(allMoviesConcat);
+
+let allMoviesFlat = allM.flat(Infinity); // flat() nemodifikuoja originalo
+console.log(allMoviesFlat);
+
+function listMoviesSo(arr) {
+  let list = document.querySelector("ul");
+
+  for (let i = 0; i < arr.length; i++) {
+    list.innerHTML += `<li>${i + 1}. ${arr[i]}</li>`;
+  }
+}
+//listMoviesSo(allMoviesFlat);
+
+// spread
+let allMoviesSpread = [...movies1, ...movies2, ...movies3];
+console.log("allMoviesSpread", allMoviesSpread);
+listMoviesSo(allMoviesSpread);
+
+// Map
+
+console.log("---------Map-------");
+let citiesArr = ["Vln", "Klp", "Kau", "Sl", "Pan"];
+
+citiesArr.map((city, index) => console.log(index + 1, city));
+
+// perdaryti funkcijas su map
+// UŽDUOTIS (papildoma):
+// 1. Sukurti 10-ies skaičių masyvą.
+// 2. Sukurti funkciją, kuri priima skaičių masyvą ir grąžina jų vidurkį.
+// 2.1. Reikšmę išsaugoti ir išvesti į ekraną (HTML elementą) tokiu formatu: „Skaičių (1, 2, 3, 5, 6 ...) vidurkis yra 15"
+// 3. Sukurti funkciją, kuri priima skaičių masyvą ir grąžina jų medianą.
+// 3.1. Reikšmę išsaugoti ir išvesti į ekraną (HTML elementą) tokiu formatu: „Skaičių (1, 2, 3, 5, 6 ...) mediana yra 15"
+// 4. Sukurti funkciją, kuri priima skaičių masyvą ir grąžina didžiausia jame esantį skaičių.
+// 4.1. Reikšmę išsaugoti ir išvesti į ekraną (HTML elementą) tokiu formatu: „Skaičių (1, 2, 3, 5, 6 ...) didžiausias skaičius yra 15"
+// 5. Sukurti funkciją, kuri priima skaičių masyvą ir grąžina mažiausią jame esantį skaičių.
+// 5.1. Reikšmę išsaugoti ir išvesti į ekraną (HTML elementą) tokiu formatu: „Skaičių (1, 2, 3, 5, 6 ...) mažiausias skaičius yra 1"
+// 6. Sukurti funkciją, kuri priima skaičių masyvą ir grąžina n-tąjį skaičių pagal dydį (skaičiuojant nuo mažiausio iki didžiausio).
+// 6.1. Reikšmę išsaugoti ir išvesti į ekraną (HTML elementą) tokiu formatu: „Skaičių (1, 2, 3, 5, 6 ...) trečias skaičius pagal dydį yra 3"
