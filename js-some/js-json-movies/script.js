68705b88cb83e048cea9faaa2ea10e023e9d28cf
Giedre <giedre.klima@gmail.com>
2022-06-28 18:32:38 +0300

    small part of movies-task

--- js-some/js-json-movies/script.js
+++ js-some/js-json-movies/script.js
@@ -30,12 +30,37 @@ fetch("movies.json")
     let moviesList = document.querySelector("#movies-list");
 
     movies.map((movie) => {
-      console.log(movie);
-      console.log(movie.title);
-      console.log(movie.releaseYear);
       let movieItem = document.createElement("div");
-      movieItem.textContent = movie.title;
+      movieItem.classList.add("movie-item");
+
+      let movieTitle = document.createElement("h3");
+      movieTitle.textContent = movie.title;
+      movieTitle.classList.add("movie-title");
+
+      let movieGenres = document.createElement("span");
+      //movie.genres.map((genre) => (movieGenres.textContent = genre));
+      movieGenres.textContent = movie.genres[0];
+
+      let movieRanking = document.createElement("span");
+      movieRanking.textContent = movie.review.ranking;
+      movieRanking.classList.add("star");
+
+      let movieViews = document.createElement("span");
+      let count = numberShort(movie.review.count);
+      movieViews.textContent = count;
+      movieViews.classList.add("views");
 
       moviesList.append(movieItem);
+      movieItem.append(movieTitle);
+      movieItem.append(movieGenres);
+
+      movieItem.append(movieRanking);
+      movieItem.append(movieViews);
     });
   });
+
+let numberShort = (number) => {
+  if (number > 1000000) return number / 1000000 + "M";
+  if (number > 1000) return number / 1000 + "K";
+  return number;
+};
