556fcfbfef87a2af478e4d402bccdf0363af475e
Giedre <giedre.klima@gmail.com>
2022-07-03 23:04:13 +0300

    cao lesson 12 done

--- js-some/js-cao-12/script.js
+++ js-some/js-cao-12/script.js
@@ -1,18 +1,18 @@
 // 12.1
 let isLegalAge = true;
-console.log(isLegalAge);
-console.log(isLegalAge.toString());
+// console.log(isLegalAge);
+// console.log(isLegalAge.toString());
 
 // 12.2.1
 let orangePrice = 1.39;
 let text = Number.isInteger(orangePrice)
   ? "you do not need cents"
   : "you need cents";
-console.log(text); // false
+//console.log(text); // false
 
 // 12.2.2
 let price = 1.576;
-console.log(price.toFixed(2));
+// console.log(price.toFixed(2));
 
 // 12.2.3
 let fuelFormEl = document.querySelector("#fuel-price-counter-form");
@@ -23,7 +23,6 @@ fuelFormEl.addEventListener("submit", (event) => {
   let price = event.target.elements.price.value;
   let amount = event.target.elements.amount.value;
   let result = price * amount;
-  console.log(result.toFixed(2));
   resultTextEl.textContent = result.toFixed(2) + " Eur";
 });
 
@@ -36,14 +35,21 @@ nameCountFormEl.addEventListener("submit", (event) => {
   let word = event.target.elements.word.value;
   let count = Number(event.target.elements.number.value);
 
-  console.log(Number.isInteger(count));
-  console.log(typeof count);
   if (!Number.isInteger(count)) alert("Number is not a full number");
   let textPlaceEl = document.querySelector("#result");
   textPlaceEl.textContent = word.repeat(count);
 });
 
 // 12.3.2
+
+let wordLengthFormEl = document.querySelector("#word-length-form");
+wordLengthFormEl.addEventListener("submit", (event) => {
+  event.preventDefault();
+  let word = event.target.elements.word.value.trim();
+  alert(word.length);
+});
+
+// 12.3.3
 let nameSurnameFormEl = document.querySelector("#nameSurnameForm");
 let fieldsetEl = document.querySelector("#fieldset-name-surname");
 nameSurnameFormEl.addEventListener("submit", (event) => {
@@ -53,8 +59,8 @@ nameSurnameFormEl.addEventListener("submit", (event) => {
   let nameEl = document.createElement("h4");
   nameEl.textContent = result[0];
   let surNameEl = document.createElement("h4");
-  surNameEl.textContent = result[1];
-  fieldsetEl.append(nameEl);
-  fieldsetEl.append(surNameEl);
-  console.log(result);
+  surNameEl.textContent = result.slice(1).join(" ");
+
+  //console.log(result.slice(1));
+  fieldsetEl.append(nameEl, surNameEl);
 });
