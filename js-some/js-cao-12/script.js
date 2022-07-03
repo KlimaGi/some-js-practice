f16dba60e37a76cbb722385edc543be8a0753c6d
Giedre <giedre.klima@gmail.com>
2022-07-03 20:01:52 +0300

    12.3.1 and 12.3.2 task done

--- js-some/js-cao-12/script.js
+++ js-some/js-cao-12/script.js
@@ -26,3 +26,35 @@ fuelFormEl.addEventListener("submit", (event) => {
   console.log(result.toFixed(2));
   resultTextEl.textContent = result.toFixed(2) + " Eur";
 });
+
+// 12.3.1
+let nameCountFormEl = document.querySelector("#word-form");
+
+nameCountFormEl.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  let word = event.target.elements.word.value;
+  let count = Number(event.target.elements.number.value);
+
+  console.log(Number.isInteger(count));
+  console.log(typeof count);
+  if (!Number.isInteger(count)) alert("Number is not a full number");
+  let textPlaceEl = document.querySelector("#result");
+  textPlaceEl.textContent = word.repeat(count);
+});
+
+// 12.3.2
+let nameSurnameFormEl = document.querySelector("#nameSurnameForm");
+let fieldsetEl = document.querySelector("#fieldset-name-surname");
+nameSurnameFormEl.addEventListener("submit", (event) => {
+  event.preventDefault();
+  let nameSurname = event.target.elements["name-surname"].value;
+  let result = nameSurname.trim().split(" ");
+  let nameEl = document.createElement("h4");
+  nameEl.textContent = result[0];
+  let surNameEl = document.createElement("h4");
+  surNameEl.textContent = result[1];
+  fieldsetEl.append(nameEl);
+  fieldsetEl.append(surNameEl);
+  console.log(result);
+});
